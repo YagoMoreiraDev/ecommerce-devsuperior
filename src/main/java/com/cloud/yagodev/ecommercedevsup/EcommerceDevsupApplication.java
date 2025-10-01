259735a9761f7e7d040604f2f5805b07e8bc7e74
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-10-01 20:57:10 -0300

    Iniciando o projeto

---
+++ src/main/java/com/cloud/yagodev/ecommercedevsup/EcommerceDevsupApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.ecommercedevsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDevsupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceDevsupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/yagodev/ecommercedevsup/EcommerceDevsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.ecommercedevsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDevsupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
