0ad2523d010469de7267a8de8d5434e904d844ec
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-10-02 14:47:51 -0300

    feat: Criado as entidades Product e Category e seus relacionamentos

---
+++ src/main/java/com/cloud/yagodev/ecommercedevsup/entities/Category.java
@@ -0,0 +1,43 @@
+package com.cloud.yagodev.ecommercedevsup.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<Product>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+}
